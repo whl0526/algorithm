b47c137e65883bd7a87972e7b239a976e78268b6
이원형 <67450169+whl0526@users.noreply.github.com>
2023-03-26 16:04:54 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func solution(_ n:Int) -> Int
+{
+   var answer : Int = n + 1
+    while true {
+        if n.nonzeroBitCount == answer.nonzeroBitCount {
+            break
+        }
+        answer += 1
+    }
+    return answer
+}
