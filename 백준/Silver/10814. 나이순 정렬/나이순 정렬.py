59e96c4b92c26a0a050df21af2a6e5d62b549281
이원형 <67450169+whl0526@users.noreply.github.com>
2023-05-13 03:35:29 +0900

    [Silver V] Title: 나이순 정렬, Time: 4240 ms, Memory: 46276 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -0,0 +1,10 @@
+n = int(input())
+member = []
+for _ in range(n):
+    age,name = map(str,input().split())
+    age = int(age)
+    member.append((age,name))
+member.sort(key = lambda x: x[0]) #(age,name)에서 age만 비교
+
+for i in member:
+    print(i[0],i[1])
