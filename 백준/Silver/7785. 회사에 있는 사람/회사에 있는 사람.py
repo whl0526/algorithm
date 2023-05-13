f82ad1e538834773162435b13a5bbca49e39f61a
이원형 <67450169+whl0526@users.noreply.github.com>
2023-05-13 22:33:36 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 232 ms, Memory: 48468 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+temp = dict()
+for _ in range(n):
+    a,b = map(str,input().split())
+    
+    if b == "enter":
+        temp[a] = b
+    else:
+        del temp[a]
+temp = sorted(temp.keys(),reverse = True)
+
+for i in temp:
+    print(i)
