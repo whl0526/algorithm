ebf45ff41b9d610d526f74e719b37c52dd6d11c9
이원형 <67450169+whl0526@users.noreply.github.com>
2023-05-21 22:38:58 +0900

    [Silver II] Title: 에디터, Time: 312 ms, Memory: 37576 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -0,0 +1,22 @@
+import sys
+
+st1 = list(sys.stdin.readline().rstrip())
+st2 = []
+
+for _ in range(int(sys.stdin.readline())):
+    command = list(sys.stdin.readline().split())
+    if command[0] == 'L':
+        if st1:
+            st2.append(st1.pop())
+    elif command[0] == 'D':
+        if st2:
+            st1.append(st2.pop())
+    elif command[0] == 'B':
+        if st1:
+            st1.pop()
+    else:
+        st1.append(command[1])
+
+st2.reverse()
+st1.extend(st2)
+print(''.join(st1))
