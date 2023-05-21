16a401322acd04d3e7ac908e8167f0e38d2196dd
이원형 <67450169+whl0526@users.noreply.github.com>
2023-05-22 00:54:12 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,13 +1,13 @@
-from collections import deque
+N,K = map(int,input().split())
+arr = [i for i in range(1,N+1)]    # 맨 처음에 원에 앉아있는 사람들
 
-def josephus(n,k):
-    circle = deque(range(1,n+1))
-    result = [] #요세푸스 순열을 저장할 리스트
-    
-    while circle:
-        circle.rotate(-(k-1))
-        result.append(circle.popleft())
-        
-    return result
-n,k = map(int,input().split())
-print("<"+", ".join(map(str,josephus(n,k)))+">")
+answer = []   # 제거된 사람들을 넣을 배열
+num = 0  # 제거될 사람의 인덱스 번호
+
+for t in range(N):
+    num += K-1  
+    if num >= len(arr):   # 한바퀴를 돌고 그다음으로 돌아올때를 대비해 값을 나머지로 바꿈  
+        num = num%len(arr)
+ 
+    answer.append(str(arr.pop(num)))
+print("<",", ".join(answer)[:],">", sep='')
