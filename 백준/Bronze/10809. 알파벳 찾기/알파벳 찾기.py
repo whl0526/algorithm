e93974a72c1729218ab6e853d76da36f13ecc546
이원형 <67450169+whl0526@users.noreply.github.com>
2023-05-23 23:18:31 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 40 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,12 +1,4 @@
-#알파벳 26개 
+S = input()
 
-        
-S = list(input())
-c = 'abcdefghijklmnopqrstuvwxyz'
-
-for i in c:
-    if i in S:
-        print(S.index(i), end =' ')
-    else:
-        print(-1, end = ' ')
-        
+for x in 'abcdefghijklmnopqrstuvwxyz':
+    print(S.find(x), end = ' ')
