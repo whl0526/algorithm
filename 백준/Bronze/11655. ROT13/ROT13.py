ae5964c8d0a30abf11e69b41fae16c1980b8cbb2
이원형 <67450169+whl0526@users.noreply.github.com>
2023-05-23 23:51:20 +0900

    [Bronze I] Title: ROT13, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/11655. ROT13/ROT13.py
@@ -0,0 +1,11 @@
+arr=input()
+ans=''
+for i in arr:
+    if i.islower():
+        print(chr(97+(ord(i)+13-97)%26), end='')
+
+    elif i.isupper():
+        print(chr(65+(ord(i)+13-65)%26), end='')
+
+    else:
+        print(i,end='')
