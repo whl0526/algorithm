f956288a40c5d7ef060bda9b33cbcf77f6de146c
이원형 <67450169+whl0526@users.noreply.github.com>
2023-05-29 18:27:15 +0900

    [Bronze II] Title: 하얀 칸, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1100. 하얀 칸/하얀 칸.py
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.py
@@ -1,11 +1,9 @@
-chess = []
+            
+
+chess = [input() for _ in range(8)]
 cnt = 0
-for _ in range(8):
-    chess.append(input())
 for i in range(8):
     for j in range(8):
-        if i % 2 == 1 and j%2 == 1 and chess[i][j] == "F":
-            cnt += 1
-        if i % 2 == 0 and j%2 == 0 and chess[i][j] == "F": 
+        if i % 2 == j % 2 and chess[i][j] == "F":
             cnt += 1
 print(cnt)            
