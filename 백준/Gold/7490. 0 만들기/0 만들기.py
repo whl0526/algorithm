fc4d3bf641d9b29bb7160b52b4782efed1a5a0ac
이원형 <67450169+whl0526@users.noreply.github.com>
2023-06-14 22:46:36 +0900

    [Gold V] Title: 0 만들기, Time: 148 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/7490. 0 만들기/0 만들기.py
@@ -0,0 +1,24 @@
+tc = int(input())
+
+def solv():
+    global n,answer
+    n = int(input())
+    answer = []
+    select_operator(2,'1')
+    print()
+def select_operator(now,ans):
+    global answer
+    if now == n+1:
+        calc(ans)
+        return
+
+    select_operator(now+1,ans+' '+str(now))
+    select_operator(now+1,ans+'+'+str(now))
+    select_operator(now+1,ans+'-'+str(now))
+
+def calc(ans):
+    tmp = ans.replace(' ','')
+    if eval(tmp) == 0:
+        print(ans)
+for _ in range(tc):
+    solv()
