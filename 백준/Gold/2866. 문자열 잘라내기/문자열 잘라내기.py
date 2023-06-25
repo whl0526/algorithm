e766ae9ae64740499e0f0d60d497bd29b33acee8
이원형 <67450169+whl0526@users.noreply.github.com>
2023-06-25 23:17:57 +0900

    [Gold V] Title: 문자열 잘라내기, Time: 1156 ms, Memory: 40404 KB -BaekjoonHub

---
+++ 백준/Gold/2866. 문자열 잘라내기/문자열 잘라내기.py
@@ -0,0 +1,30 @@
+# 문자열 잘라내기
+from collections import defaultdict
+R, C = map(int, input().split())
+arr = [list(input()) for _ in range(R)]
+
+result = 0
+start, end = 0, R-1
+while start <= end:
+    mid = (start+end)//2
+
+    # 문자열 중복 확인
+    isOK = True
+    dict = defaultdict(int)
+    for j in range(C):
+        temp = ''
+        for i in range(mid, R):
+            temp += str(arr[i][j])
+        if not dict[temp]:
+            dict[temp] += 1
+        else:
+            isOK = False
+            break
+
+    if isOK:
+        result = mid
+        start = mid + 1
+    else:
+        end = mid - 1
+
+print(result)
