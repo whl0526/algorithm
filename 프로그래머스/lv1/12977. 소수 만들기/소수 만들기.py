67b6a3c10a010dd3d15436d900f0457d780c652e
이원형 <67450169+whl0526@users.noreply.github.com>
2023-07-01 16:27:13 +0900

    [level 1] Title: 소수 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
@@ -0,0 +1,22 @@
+from itertools import combinations
+
+#소수 판별 함수
+def is_prime_number(num):
+    if num == 0 or num == 1:
+        return False
+    else:
+        for n in range(2,(num//2)+1):
+            if num % n == 0:
+                return False
+        return True
+    
+def solution(nums):
+    answer = 0
+    cmb = list(combinations(nums,3))
+    for arr in cmb:
+        if is_prime_number(sum(arr)):
+            answer += 1
+
+    
+
+    return answer
