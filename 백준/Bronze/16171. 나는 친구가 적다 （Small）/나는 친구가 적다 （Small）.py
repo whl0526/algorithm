dd15ff9a12574b10ef875346cb40ac92e41b47c7
이원형 <67450169+whl0526@users.noreply.github.com>
2023-08-29 23:28:56 +0900

    [Bronze II] Title: 나는 친구가 적다 (Small), Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/16171. 나는 친구가 적다 （Small）/나는 친구가 적다 （Small）.py
@@ -0,0 +1,18 @@
+S = list(input())
+K = input()
+new = []
+
+# 알파벳 대문자 혹은 소문자일 때만 새로운 배열 new에 append
+for s in S:
+    if ord(s) >= 65 and ord(s) < 91:      # 알파벳 대문자인지 판별
+        new.append(s)
+    elif ord(s) >= 97 and ord(s) < 123:   # 알파벳 소문자인지 판별
+        new.append(s)
+
+new = ''.join(new)
+
+if K in new:
+    print(1)
+else:
+    print(0)
+    
