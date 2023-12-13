2a80f77ac0f9c66f788b039ca5e44f1a17a68e80
이원형 <67450169+whl0526@users.noreply.github.com>
2023-12-13 23:06:22 +0900

    [Silver III] Title: 놀라운 문자열, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1972. 놀라운 문자열/놀라운 문자열.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+while True:
+    s = input().rstrip()
+    if s == "*":
+        break
+    
+    for d in range(1,len(s)-1):
+        check = set()
+        for i in range(len(s)-d):
+            pairs = s[i]+s[i+d]
+            if pairs in check:
+                print(s,"is NOT surprising.")
+                break
+            else:
+                check.add(pairs)
+        else:
+            continue
+        break
+    else:
+        print(s,"is surprising.")
