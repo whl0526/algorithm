33589ff0cb38d798c73a34273a0a25f596590531
이원형 <67450169+whl0526@users.noreply.github.com>
2024-01-13 23:31:33 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 64 ms, Memory: 34016 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
@@ -1,37 +1,25 @@
-import sys
 from collections import Counter
 
-word = list(map(str, sys.stdin.readline().strip()))
-word.sort() # 사전순으로 정렬하기 위해 오름차순 정렬
-check = Counter(word) # 홀수의 개수를 확인하기 위해 Counter 함수 사용
+# 문자열 입력 받기
+string = input()
+counter = Counter(string)
 
-cnt = 0 # 홀수의 개수
-center = "" # 홀수의 문자
+# 팰린드롬 생성을 위한 변수들
+mid = ""
+result = ""
+odd_count = 0
 
-# 반복문을 통해 각 문자의 개수를 확인
-for i in check:
-    # 문자의 개수가 홀수일 경우
-    # 홀수의 개수를 카운트하고 홀수의 문자를 더해준다.
-    if check[i] % 2 != 0:
-        cnt += 1
-        center += i
-        word.remove(i) # 홀수의 문자 하나를 문자열에서 제거
+# 문자 빈도 수에 따라 팰린드롬 생성
+for char, freq in sorted(counter.items()):
+    if freq % 2 != 0:
+        mid = char
+        odd_count += 1
+        freq -= 1
+    result += char * (freq // 2)
 
-    # 홀수의 개수가 1보다 클 경우 팰린드롬으로 바꾸지 못한다.
-    if cnt > 1:
-        break
-
-# 홀수의 개수가 1보다 클 경우 팰린드롬으로 바꾸지 못한다.
-if cnt > 1:
+# 팰린드롬 가능 여부 확인
+if odd_count > 1:
     print("I'm Sorry Hansoo")
-
-# 홀수의 개수가 1 이하일 경우에는 팰린드롬으로 바꿀 수 있다.
 else:
-
-    res = ""
-    # 반복문을 통해 팰린드롬을 반을 나누었을 때 왼쪽 부분을 더해준다.
-    for i in range(0, len(word), 2):
-        res += word[i]
-
-    # 왼쪽 + 가운데(홀수) + 오른쪽
-    print(res + center + res[::-1])
+    # 팰린드롬 출력
+    print(result + mid + result[::-1])
