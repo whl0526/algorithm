a1405354f23f5ed1f8d37d5b638ca3a4d46075df
이원형 <67450169+whl0526@users.noreply.github.com>
2024-01-14 12:32:27 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,14 +1,8 @@
+expression = input().split('-')
 
+result = sum(map(int,expression[0].split('+')))
 
-a = input().split('-')
-num = []
-for i in a:
-    cnt = 0
-    s = i.split('+')
-    for j in s:
-        cnt += int(j)
-    num.append(cnt)
-n = num[0]
-for i in range(1,len(num)):
-    n -= num[i]
-print(n)    
+for exp in expression[1:]:
+    result -= sum(map(int,exp.split('+')))
+print(result)
+
