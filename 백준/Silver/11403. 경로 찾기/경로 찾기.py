27a1715e9983cad2e04a4bd8d28c0c180d3ecc62
이원형 <67450169+whl0526@users.noreply.github.com>
2024-01-15 23:52:43 +0900

    [Silver I] Title: 경로 찾기, Time: 220 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -0,0 +1,13 @@
+n = int(input())
+graph = [list(map(int, input().split())) for _ in range(n)]
+
+# 플로이드-워셜 알고리즘 적용
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            if graph[i][k] and graph[k][j]:
+                graph[i][j] = 1
+
+# 결과 출력
+for row in graph:
+    print(' '.join(map(str, row)))
