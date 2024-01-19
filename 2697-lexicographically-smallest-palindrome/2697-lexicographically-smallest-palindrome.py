9aa539039224785710e1a1ce4c08056ce629e0d3
이원형 <67450169+whl0526@users.noreply.github.com>
2024-01-19 23:35:10 +0900

    Time: 102 ms (85.31%), Space: 16.6 MB (35.24%) - LeetHub

--- 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.py
+++ 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.py
@@ -1,16 +1,18 @@
 class Solution:
     def makeSmallestPalindrome(self, s: str) -> str:
-        char = list(s)
-        i = 0
-        j = len(s) - 1 #인덱스 값은 0부터 시작이기 때문에
         
-        while i < j: 
-            minChar = min(char[i], char[j])
-            char[i] = minChar
-            char[j] = minChar
+        char = list(s)
+        
+        start = 0
+        end = len(s) - 1 #인덱스 값은 0부터 시작이기 때문에
+        
+        while start < end: 
+            minChar = min(char[start], char[end])
+            char[start] = minChar
+            char[end] = minChar
             
-            i += 1
-            j -= 1
+            start += 1
+            end -= 1
             
         return ''.join(char)
             
