ede541250b2094f84d7ccb02b5573ce345b8baf6
이원형 <67450169+whl0526@users.noreply.github.com>
2024-01-24 23:54:46 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+N, M = list(map(int,input().split()))
+
+s = []
+
+def backtracking(start):
+    if len(s) == M:
+        print(' '.join(map(str,s)))
+        return 
+    for i in range(start, N+ 1):
+        if i not in s:
+            s.append(i)
+            backtracking(i+1)
+            s.pop()
+
+backtracking(1)            
