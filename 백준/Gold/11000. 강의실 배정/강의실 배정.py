49de133cacddc3280b8aa7648089ca073a9d188c
이원형 <67450169+whl0526@users.noreply.github.com>
2024-02-01 23:29:02 +0900

    [Gold V] Title: 강의실 배정, Time: 360 ms, Memory: 74228 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.py
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -4,7 +4,7 @@ import heapq
 input = sys.stdin.readline
 
 n = int(input())
-time = [list(map(int,input().split())) for _ in range(n)]
+time = [list(map(int, input().split())) for _ in range(n)]
 
 time.sort(key=lambda x : x[0])
 
