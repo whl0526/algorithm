736440c4f852364966323556019fb17436dbc3ad
이원형 <67450169+whl0526@users.noreply.github.com>
2024-05-07 23:28:24 +0900

    [Gold V] Title: 택배 배송, Time: 240 ms, Memory: 51424 KB -BaekjoonHub

---
+++ 백준/Gold/5972. 택배 배송/택배 배송.py
@@ -0,0 +1,30 @@
+# 택배 배송
+import heapq
+import sys
+input = sys.stdin.readline
+N, M = map(int, input().split())
+arr = [[]for _ in range(N+1)]
+for _ in range(M):
+    a, b, cost = map(int, input().split())
+    arr[a].append((cost, b))
+    arr[b].append((cost, a))
+
+
+def dijkstra():
+    q = []
+    heapq.heappush(q, (0, 1))
+    total = [sys.maxsize] * (N+1)
+    total[1] = 0
+    while q:
+        cost, node = heapq.heappop(q)
+        if node == N:
+            return total[node]
+        if total[node] < cost:
+            continue
+        for ncost, nnode in arr[node]:
+            if cost+ncost < total[nnode]:
+                total[nnode] = cost+ncost
+                heapq.heappush(q, (cost+ncost, nnode))
+
+
+print(dijkstra())
